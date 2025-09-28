72da27dc1e8342f140aa128e42b3838e62494b2e
Istiak <new.life3618@gmail.com>
2025-09-28 08:25:50 +0600

    quick fix: wrong quatation in match pattern and change comments format

--- log_monitor/monitor_logs.sh
+++ log_monitor/monitor_logs.sh
@@ -7,24 +7,20 @@
 
 set -eo pipefail
 
-# Define log file
+# === CONFIGURATION ===
 LOG_FILE="/var/log/syslog"
-
-# Define keywords to search
 KEYWORDS="error|failed|denied"
-
-# Define output file
 ALERT_FILE="alerts.log"
 
-# add timestamp each entry
-# Search last 100 line to search keywords and appead to alerts.log
-MATCHES=$(tail -n 100 "$LOG_FILE" | grep -iE "$KEYWORDS)"
+# === FIND AND STORE RESULT IN VARIABLE ===
+MATCHES=$(tail -n 100 "$LOG_FILE" | grep -iE "$KEYWORDS")
 
+# === STORE RESULT IN A FILE ===
 if [[ -n "$MATCHES" ]]; then
 	echo "[$(date)] checking $LOG_FILE for alerts..." >> "$ALERT_FILE"
 	echo "$MATCHES" >> "$ALERT_FILE"
 
-	# send notification alerts to mail
+# === SEND EMAIL ALERT ===
 	if command -v mail >/dev/null 2>&1; then 
 		echo "$MATCHES" | mail -s "[$(date +%Y-%m-%d)] Log Alert from $(hostname)" alertmail@gmail.com
 	else 
