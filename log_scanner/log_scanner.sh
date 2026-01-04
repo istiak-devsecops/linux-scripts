3b04525db947dc5978317ea71a402a2070822ac2
MD ISTIAK AHMED <istiak.ahmed.devsecops@gmail.com>
2026-01-04 22:48:32 +0600

    script: Incident Report Generator

---
+++ log_scanner/log_scanner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####################################
+# Author: Istiak Ahmed             #
+# Date: 4-January-2026             #
+# Task: Incident Report Generator  #
+####################################
+
+# ==== safety header ====
+set -euo pipefail
+
+
